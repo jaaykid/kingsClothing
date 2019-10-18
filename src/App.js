b9dcf04aea5d9ddc18cf518ba72bf5e5ac497030
jaay_kid <justincurrydev@gmail.com>
2019-10-17 18:42:58 -0700

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Homepage from './homepage.component'
+
 function App() {
   return (
-    <div>
-      hello world 
-    </div>
+    <Homepage />
   );
 }
 
